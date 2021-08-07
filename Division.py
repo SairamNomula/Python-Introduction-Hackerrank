332584557869d24d5f641923811014d453140fae
Sairam Nomula <78247889+SairamNomula@users.noreply.github.com>
2021-08-07 16:29:30 +0530

    Create Division.py

---
+++ Division.py
@@ -0,0 +1,11 @@
+# Python Division
+# Task
+# The provided code stub reads two integers, a and b, from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a /b .
+# No rounding or formatting is necessary.
+
+a = int(raw_input())
+b = int(raw_input())
+
+print a/b
+print float(a) / b

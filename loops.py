8d7ae52873fa8aea12c85e2ab04819ff2bc9b554
Sairam Nomula <78247889+SairamNomula@users.noreply.github.com>
2021-08-07 16:29:49 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,9 @@
+# Python Loops
+# Task
+# The provided code stub reads and integer, n, from STDIN. For all non-negative integers i<n, print i^2.
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    
+    for i in range(n):
+        print i * i
